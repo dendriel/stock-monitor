302fd9b54ae55dbb4fb4a44ef2d9572f0ab8924e
vitor.rozsa <vitor.rozsa@hotmail.com>
2021-12-26 09:31:53 -0300

    Deploy VPC related infra via terraform

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "stock-monitor" {
 
   tags = {
     Name        = "Stock Monitor"
-    Environment = "Prod"
+    Environment = "prod"
   }
 }
 
--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,5 +1,10 @@
 resource "aws_sns_topic" "notifications" {
   name = var.topic
+
+  tags = {
+    Name        = "Stock Monitor"
+    Environment = "prod"
+  }
 }
 
 resource "aws_sns_topic_subscription" "email-target" {
